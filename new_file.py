b132371c823581b2d079290a3435e6b8fbc7ee56
Volodymyr Bojko <vbojko86@gmail.com>
2020-01-18 13:39:21 +0200

    add pre-commit

--- new_file.py
+++ new_file.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 n_pts = 100
 np.random.seed(0)
-top_region = np.array([np.random.normal(10, 2, n_pts), np.random.normal(12, 2, n_pts)]).T
+top_region = np.array([np.random.normal(10, 2, n_pts), np.random.normal(12, 2, n_pts)]).T #ksdflkajshdfkjashdfkljhasdlkfjhaslkdfhalksdhfasdf
 bottom_region = np.array([np.random.normal(5, 2, n_pts), np.random.normal(6, 2, n_pts)]).T
 _, ax = plt.subplots(figsize=(4, 4))
 ax.scatter(top_region[:, 0], top_region[:, 1], color='r')
